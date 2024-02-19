56ccf7df976608524bf4ca1a8bedbb4818efdb06
Julia Veenkamp <juliaveenkamp@Julias-MacBook-Pro.local>
2024-02-19 17:35:22 -0500

    [Grade Me 2]

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Route sfor JellyCats Controller
+  root to: 'jelly_cats#index'
+  # Routes for JellyCats Controller
   resources :jelly_cats do
     resources :ratings
   end
